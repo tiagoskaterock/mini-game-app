6bb6759dff463da65e476fb70675e790df6a3eb1
Tiago Bordin <tiagolemespalhano@gmail.com>
2024-07-09 02:30:41 -0300

    48. Setting Up our Screen Components

---
+++ screens/StartGameScreen.jsx
@@ -0,0 +1,10 @@
+import { TextInput, View } from 'react-native';
+
+function StartGameScreen() {
+    return <View>
+        <TextInput />
+        {/* The buttons go here */}
+    </View>
+}
+
+export default StartGameScreen;
