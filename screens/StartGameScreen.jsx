0b842a0d3d71eacda4e1232139906cd24fd7e213
Tiago Bordin <tiagolemespalhano@gmail.com>
2024-07-15 13:57:49 -0300

    50. Styling for Android & iOS

--- screens/StartGameScreen.jsx
+++ screens/StartGameScreen.jsx
@@ -13,11 +13,17 @@ function StartGameScreen() {
 }
 
 const styles = StyleSheet.create({
-    inputContainer: {
-        
+    inputContainer: {        
         marginTop: 100,
+        marginHorizontal: 24,
         padding: 16,
         backgroundColor: '#72063c',
+        borderRadius: 8,
+        // elevation: 4,
+        shadowColor: 'black',
+        shadowOffset: {width: 0, height: 2},
+        shadowRadius: 6,
+        shadowOpacity: 1,
     }
 });
 
